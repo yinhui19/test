5775c5048bb20bb069cbb1d5217e85c434621a06
yinhui19 <1069978433@qq.com>
2018-05-03 17:37:54 +0800

    -

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireLess = require('react-app-rewire-less');
 
@@ -8,15 +7,3 @@ module.exports = function override(config, env) {
      modifyVars: { "@primary-color": "#2e8b57" },
    })(config, env);
     return config;
-=======
-const { injectBabelPlugin } = require('react-app-rewired');
-const rewireLess = require('react-app-rewire-less');
-
-module.exports = function override(config, env) {
-   config = injectBabelPlugin(['import', { libraryName: 'antd', style: true }], config);
-   config = rewireLess.withLoaderOptions({
-     modifyVars: { "@primary-color": "#2e8b57" },
-   })(config, env);
-    return config;
->>>>>>> e73b37384cdfa7bef3aee03970ef88dd3869ebb7
-  };
